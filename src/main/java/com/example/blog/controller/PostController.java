1ce0031d725b1ab7bce3149b05bcb8d8574ddb21
ruimmatos <rui.mateus.matos@gmail.com>
2023-06-24 19:44:09 +0100

    Added Paging and Sorting to Posts

--- src/main/java/com/example/blog/controller/PostController.java
+++ src/main/java/com/example/blog/controller/PostController.java
@@ -2,7 +2,9 @@ package com.example.blog.controller;
 
 import com.example.blog.entity.Post;
 import com.example.blog.payload.PostDto;
+import com.example.blog.payload.PostResponse;
 import com.example.blog.service.PostService;
+import com.example.blog.utils.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,9 +26,13 @@ public class PostController {
     }
 
     @GetMapping
-    public ResponseEntity<List<PostDto>> getAllPosts(){
-        List<PostDto> posts = postService.getAllPosts();
-        return new ResponseEntity<>(posts, HttpStatus.OK);
+    public PostResponse getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIR, required = false) String sortDir
+    ){
+        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
     @PostMapping
--- src/main/java/com/example/blog/service/PostService.java
+++ src/main/java/com/example/blog/service/PostService.java
@@ -2,13 +2,14 @@ package com.example.blog.service;
 
 import com.example.blog.entity.Post;
 import com.example.blog.payload.PostDto;
+import com.example.blog.payload.PostResponse;
 
 import java.util.List;
 
 public interface PostService {
     PostDto createPost(PostDto postDto);
 
-    List<PostDto> getAllPosts();
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
 
     PostDto getPostById(Long id);
 
